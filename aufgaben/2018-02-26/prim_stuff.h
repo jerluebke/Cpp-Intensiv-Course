f09fb50088d8effe6b0fd90fbacc5ff13d0872ba
Jeremiah Lübke <jer.luebke@web.de>
2018-02-26 14:59:22 +0100

    FINAL - primzahltest

---
+++ aufgaben/2018-02-26/prim_stuff.h
@@ -0,0 +1,7 @@
+//some functions for the tasks concerning prim numbers
+#ifndef PRIM_STUFF_H
+#define PRIM_STUFF_H
+
+bool is_prim(unsigned int n);
+
+#endif
--- aufgaben/2018-02-26/primzahltest.cpp
+++ aufgaben/2018-02-26/primzahltest.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "../util.h"
-//#include "stdafx.h"
 
 using namespace std;
 
@@ -20,49 +19,31 @@ bool is_prim(unsigned int n)
 
 int main(int argc, char** argv)
 {
-    if (!n_arg(argc, 2))
+    int a  = 0;
+
+    if (!n_arg(argc, 1))
         return 0;
 
-    //#ifdef DEBUG
-    cout << "converting to int..." << endl;
-    //#endif
-    /*
     try
     {
-        int a = arg_to_int(argv[1]);
+        a = arg_to_int(argv[1]);
     }
     catch (const invalid_argument& ex)
     {
-        cout << ex.what() << endl;
+        cerr << ex.what() << endl;
         return 0;
     }
-    */
-    int a = arg_to_int(argv[1]);
 
-    if (a == -1)
-    {
-        cout << "Not an Integer..." << endl;
-        return 1;
-    }
-
-    //#ifdef DEBUG
-    cout << "checking if " << a << " is positiv..." << endl;
-    //#endif
     if (!int_is_pos(a))
         return 0;
 
     unsigned n = a;
-
-    //#ifdef DEBUG
-    cout << "checking if " << n << " is prim..." << endl;
-    //#endif
     if (!is_prim(n))
     {
         cout << a << " is not prim..." << endl;
         return 0;
     }
-
     cout << a << " is prim!" << endl;
-	cin.get();
+
     return 0;
 }
